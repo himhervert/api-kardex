eefb35a3b13c1572b559b6353bbe2ffbafd09a50
himhervert <leohervert@gmail.com>
2018-10-11 10:03:13 -0500

    Clase 11 de octubre

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # api-kardex
 Ejemplo de un API REST para el kardex de calificaciones.
+
+## Autentificación y Autorización
+
+### JSON Web Token
+
+Cadena formada por:
+
+1) Encabezado
+    1) Tipo de Token
+    2) Algoritmo de Codificación
+2) Payload
+    1) Datos
+3) Firma de Verificación
+
+### Node
+
+    npm install jsonwebtoken
+    var jwt = require('jsonwebtoken');
+    jwt.sign(payload,clave,options);
+
+    |Cliente|   ----/login--->  |API|
+                <---token-----  Genera token
+                                (Verificar)
+
+REST Significa que no hay representacion real del cliente, 
+cada peticion se genera en el servidor.
--- servidor.js
+++ servidor.js
@@ -1,28 +1,73 @@
-
 var express = require('express');
 var jwt = require('jsonwebtoken');
 var puerto = process.env.PORT || 3000;
 
 var app = express();
 
-app.use(express.json());
+app.use(express.json());// Middleware para recibir ratos con formato JSON
 
-app.get('/calificaciones',function(req,res){
-    res.json({
-        mensaje: 'Bienvenido al APi del kardex'
-    });
+app.get('/calificaciones', function (req, res) {
+    console.log('Token recibido: ' + req.query.token);
+    jwt.verify(req.query.token, 'clavesupersecreta',
+        function (error, token) {
+            if (error) {
+                res.status(403).json({ mensaje: 'Autorización no valida' });
+            }
+            else {
+                res.json({
+                    mensaje: 'Bienvenido ' + token.usuario +' Aqui estan las calificaciones...'
+                });
+            }
+        });
 });
-app.post('/login',function(req,res){
+app.post('/login', function (req, res) {
+    // Simular la base de datos
+    var alumno = {
+        email: 'alumno@uaslp.mx',
+        password: '1234'
+    };
 
-    var token = jwt.sign({
-        usuario :'alumno',
-    },'clavesupersecreta',{expiresIn:'60s'});//////// NUNCA MANDAR PASSWORD EN EL TOKEN
-    console.log('Token generado: '+ token);
-    res.json({
-        elToken: token
-    });
+    var profesor = {
+        email: 'profesor@uaslp.mx',
+        password: 'abc'
+    }
+    if (req.body.email == alumno.email &&
+        req.body.password == alumno.password) {
+        var token = jwt.sign({
+            usuario: 'alumno',
+            nombre: 'Leito',
+            claveUnica: 123456
+        }, 'clavesupersecreta', { expiresIn: '1h' });//////// NUNCA MANDAR PASSWORD EN EL TOKEN
+        console.log('Token generado: ' + token);
+        res.json({
+            mensaje: 'Bienvenido alumno',
+            elToken: token
+        });
+    }
+    else {
+        if (req.body.email == profesor.email &&
+            req.body.password == profesor.password) {
+            var token = jwt.sign({
+                usuario: 'julio',
+                nombre: 'profe',
+                claveUnica: 123456
+            }, 'clavesupersecreta', { expiresIn: '1h' });//////// NUNCA MANDAR PASSWORD EN EL TOKEN
+            console.log('Token generado: ' + token);
+            res.json({
+                mensaje: 'Bienvenido profesor',
+                elToken: token
+            });
+        }
+        else {
+            res.status(401);
+            res.json({
+                mensaje: 'Credenciales no válidas',
+                elToken: null
+            });
+        }
+    }
 });
 
-app.listen(puerto,function(){
+app.listen(puerto, function () {
     console.log('Servidor corriendo en el puerto: ' + puerto);
 });
